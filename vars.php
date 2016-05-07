b415a63ab54d326829357e05597585b4f5ac9fab
jgilfn <jgilfn@gmail.com>
2016-05-07 16:52:01 +0100

    Created vars.php template

---
+++ vars.php
@@ -0,0 +1,7 @@
+<?php
+
+$apikey = "<insert your API Key here>";
+
+$con = mysqli_connect("localhost", "<insert your DB User here>", "<insert your DB Password here>", "<insert your DB Name here>");
+
+?>
